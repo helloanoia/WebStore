842c1bf85a78bbe05c69363111619500111687a5
helloanoia <girya96@gmail.com>
2019-07-03 21:46:15 +0500

    Add project files.

---
+++ WebStore/Controllers/CustomerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.ViewModels;
+
+namespace WebStore.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public CustomerController()
+        {
+            
+        }
+
+        private readonly List<EmployeeView> _employees = new List<EmployeeView>
+        {
+            new EmployeeView
+            {
+                Id = 1,
+                FirstName = "Иван",
+                SurName = "Иванов",
+                Patronymic = "Иванович",
+                Age = 22
+            },
+            new EmployeeView
+            {
+                Id = 2,
+                FirstName = "Владислав",
+                SurName = "Петров",
+                Patronymic = "Иванович",
+                Age = 35
+            }
+        };
+
+        // GET
+        public IActionResult Index()
+        {
+            return View(_employees);
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(_employees.FirstOrDefault(x => x.Id == id));
+        }
+
+    }
+}
---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Домашняя страница";
+            return View();
+        }
+
+        public IActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}
